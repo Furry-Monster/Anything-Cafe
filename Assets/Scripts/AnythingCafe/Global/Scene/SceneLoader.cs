d2a563c6c851c88a4f445c090b5ccec0ee8d729c
FurryMonster <2876231904@qq.com>
2025-02-02 16:11:09 +0800

    修改一下结构

--- Assets/Scripts/AnythingCafe/Global/Scene/SceneLoader.cs
+++ Assets/Scripts/AnythingCafe/Global/Scene/SceneLoader.cs
@@ -5,19 +5,6 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 /// <summary>
-/// 场景枚举,手动修改。
-/// 游戏打包时，记得在发布中勾选所有场景，并按SceneEnum中的顺序排序
-/// 每次创建新场景的时候，在SceneEnum中添加一个新的枚举值，并添加一个新的SceneHandler脚本
-/// </summary>
-public enum SceneEnum
-{
-    TitleScene,
-    PlayScene,
-    ScenarioScene,
-    GalleryScene,
-}
-
-/// <summary>
 /// 异步场景加载器
 /// </summary>
 public class SceneLoader

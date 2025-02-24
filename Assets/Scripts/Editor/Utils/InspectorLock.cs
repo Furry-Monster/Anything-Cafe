f65b9c5e35b5f467722464aa0c4ffba4d3b84ce0
FurryMonster <2876231904@qq.com>
2025-02-24 15:12:51 +0800

    加了一些辅助工具

---
+++ Assets/Scripts/Editor/Utils/InspectorLock.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEditor;
+
+public class InspectorLock
+{
+    [MenuItem("GameObject/InspectorLock/Lock", false, 0)]
+    private static void Lock() => SetHideFlags(HideFlags.NotEditable);
+
+    [MenuItem("GameObject/InspectorLock/UnLock", false, 1)]
+    private static void UnLock() => SetHideFlags(HideFlags.None);
+
+    // Ëø¶¨GUI
+    private static void SetHideFlags(HideFlags hideFlags)
+    {
+        if (Selection.gameObjects != null)
+        {
+            foreach (var gameObject in Selection.gameObjects)
+            {
+                gameObject.hideFlags = hideFlags;
+                EditorUtility.SetDirty(gameObject);
+            }
+        }
+    }
+}

635220d07755103610ae5eba4dadaae8a8e311da
FurryMonster <2876231904@qq.com>
2025-02-25 17:51:24 +0800

    修改一点

---
+++ Assets/Scripts/Utils/Storage/Local/SaveFileConstants.cs
@@ -0,0 +1,6 @@
+public static class SaveFileConstants
+{
+    public const string Options = "Options.es3";
+    public const string AutoSave = "AutoSave.es3";
+    public const string Achievements = "Achievements.es3";
+}
